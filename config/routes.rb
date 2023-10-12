b7bbaecf6bab81fee427510d58d590768d99eb24
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-12 08:25:15 +0000

    Finished アプリケーションを完成させよう2 10章

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get "homes/about" => "homes#about"
   get "homes/about", to: "homes#about", as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
+  devise_for :users
+
   root to: "homes#top"
 end
